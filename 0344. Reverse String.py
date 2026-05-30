647c2005de774d30dd55c4e8da9842a18b2e21fe
Kartavya Jain <99114718+Kartavya-Jain@users.noreply.github.com>
2026-05-30 12:01:53 +0000

    0344. Reverse String.py

--- 0344. Reverse String.py
+++ 0344. Reverse String.py
@@ -4,4 +4,17 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
+        l=0
+        r=len(s)-1
+        while(l<r):
+            s[l],s[r]=s[r],s[l]
+            l+=1
+            r-=1
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
         s.reverse()
