83fb507ca60737f4a356a3b3f709c226a3ccb17e
Kartavya Jain <99114718+Kartavya-Jain@users.noreply.github.com>
2026-06-04 04:26:40 +0000

    0069. Sqrt(x).py

---
+++ 0069. Sqrt(x).py
@@ -0,0 +1,10 @@
+import math
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        x=math.sqrt(x)
+        x=int(x)
+        return x
